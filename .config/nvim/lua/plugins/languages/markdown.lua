d6ec185377ec7fe7a4e94cca89e4a48a84b99f8a
Stéphan Zych <szych@cbtw.tech>
2024-09-24 17:21:31 +0200

    Update nvim markdown config

--- .config/nvim/lua/plugins/languages/markdown.lua
+++ .config/nvim/lua/plugins/languages/markdown.lua
@@ -3,9 +3,12 @@ return {
   -- https://github.com/iamcco/markdown-preview.nvim
   {
     "iamcco/markdown-preview.nvim",
-    build = " cd app && npm install",
+    build = "cd app && npm install",
     enabled = true,
     ft = "markdown",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+    end,
   },
 
   -- pandoc integration and utilities for vim
@@ -16,6 +19,16 @@ return {
       "vim-pandoc/vim-pandoc-syntax",
       lazy = true,
     },
+    -- init = function()
+    --   vim.g.pandoc_filetypes_handled = { "pandoc", "markdown" }
+    --   vim.g.pandoc_filetypes_pandoc_markdown = 0
+    -- end,
+    -- keys = {
+    --   "<leader>nP",
+    --   "<cmd>Pandoc revealjs -s --mathjax -i<CR>", -- "--include-in-header=style.css -V theme=white"
+    --   desc = "Generate Presentation",
+    --   mode = "n",
+    -- },
   },
 
   -- A neovim plugin leveraging pandoc to help you convert your markdown files. It takes pandoc options from yaml blocks.
@@ -24,5 +37,18 @@ return {
     "jghauser/auto-pandoc.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
     ft = "markdown",
+  },
+
+  -- TODO: check https://mfontanini.github.io/presenterm/
+
+  -- Slides presentation in your Neovim
+  -- https://github.com/aspeddro/slides.nvim
+  -- NOTE: use `chafa` to display images (see https://github.com/maaslalani/slides/issues/2#issuecomment-1751728049)
+  {
+    "aspeddro/slides.nvim",
+    config = function(_, opts)
+      require("slides").setup(opts)
+    end,
+    opts = {},
   },
 }
