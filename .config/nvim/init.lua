769c3a1aeb39174e28ad0f00d8086786bc69338f
Stéphan Zych <szych@cbtw.tech>
2024-10-17 23:57:24 +0200

    Cleanup nvim init

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -69,7 +69,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.coding.codeium" },
     -- { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "lazyvim.plugins.extras.coding.yanky" },
-    { import = "lazyvim.plugins.extras.lang.cmake" },
+    -- { import = "lazyvim.plugins.extras.lang.cmake" },
     { import = "lazyvim.plugins.extras.lang.docker" },
     -- { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.json" },
