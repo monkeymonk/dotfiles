fffb7d1206aa8b8b94a24e89daeaed591c312795
Stéphan Zych <szych@cbtw.tech>
2024-10-19 13:53:28 +0200

    Update LazyVim haskell settings

---
+++ .config/LazyVim/lua/plugins/haskell.lua
@@ -0,0 +1,6 @@
+return {
+  -- Neotest adapter for Haskell (cabal or stack) with support for Sydtest, Hspec and Tasty
+  -- https://github.com/mrcjkb/neotest-haskell/
+  -- @see https://github.com/mrcjkb/neotest-haskell/issues/179
+  { "mrcjkb/neotest-haskell", tag = "2.0.0" },
+}
