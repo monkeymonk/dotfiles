9f6ee2a6fc22f694f8cb723b7483f636e581f046
Stéphan Zych <szych@cbtw.tech>
2024-10-19 13:53:28 +0200

    Update LazyVim haskell settings

--- .config/LazyVim/lua/config/lazy.lua
+++ .config/LazyVim/lua/config/lazy.lua
@@ -48,6 +48,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.astro" },
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.git" },
+    { import = "lazyvim.plugins.extras.lang.haskell" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.php" },
