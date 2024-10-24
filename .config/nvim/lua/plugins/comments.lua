b620afa3a571216646c71e07ef1f737f6029b437
Stéphan Zych <szych@cbtw.tech>
2024-10-24 13:50:55 +0200

    Update nvim, tmux and wezterm to work together

---
+++ .config/nvim/lua/plugins/comments.lua
@@ -0,0 +1,40 @@
+return {
+  -- Create annotations with one keybind, and jump your cursor in the inserted annotation
+  -- https://github.com/danymat/neogen
+  {
+    "danymat/neogen",
+    config = true,
+    dependencies = "nvim-treesitter/nvim-treesitter",
+    keys = {
+      {
+        "gcC",
+        function()
+          require("neogen").generate({})
+        end,
+        desc = "Neogen Comment",
+      },
+      {
+        "gC",
+        function()
+          require("neogen").generate({})
+        end,
+        desc = "Neogen Comment",
+        mode = "v",
+      },
+    },
+    opts = {
+      snippet_engine = "luasnip",
+    },
+  },
+
+  -- Smart and powerful comment plugin for neovim. Supports treesitter, dot repeat, left-right/up-down motions, hooks, and more
+  -- https://github.com/numToStr/Comment.nvim
+  {
+    "numToStr/Comment.nvim",
+    dependencies = "JoosepAlviste/nvim-ts-context-commentstring",
+    event = { "BufReadPre", "BufNewFile" },
+    opts = {
+      pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+    },
+  },
+}
