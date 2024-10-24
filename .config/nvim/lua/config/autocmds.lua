ba8dc40281d35519027f971db7f095765de3d92b
Stéphan Zych <szych@cbtw.tech>
2024-10-24 16:09:32 +0200

    Update neovim dashboard

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -13,25 +13,25 @@ autocmd("FileType", function()
   vim.wo.conceallevel = 0
 end, { pattern = { "json", "jsonc", "markdown" } })
 
--- -- Autocommand to temporarily change 'blade' filetype to 'php' when opening for LSP server activation
--- autocmd({ "BufRead", "BufNewFile" }, function()
---   vim.bo.filetype = "php"
--- end, { group = "lsp_blade_workaround", pattern = "*.blade.php" })
---
--- -- Additional autocommand to switch back to 'blade' after LSP has attached
--- autocmd("LspAttach", function(args)
---   vim.schedule(function()
---     -- Check if the attached client is 'intelephense'
---     for _, client in ipairs(vim.lsp.get_active_clients()) do
---       if client.name == "intelephense" and client.attached_buffers[args.buf] then
---         vim.api.nvim_buf_set_option(args.buf, "filetype", "blade")
---         -- update treesitter parser to blade
---         vim.api.nvim_buf_set_option(args.buf, "syntax", "blade")
---         break
---       end
---     end
---   end)
--- end, { pattern = "*.blade.php" })
+--[[ -- Autocommand to temporarily change 'blade' filetype to 'php' when opening for LSP server activation
+autocmd({ "BufRead", "BufNewFile" }, function()
+  vim.bo.filetype = "php"
+end, { group = "lsp_blade_workaround", pattern = "*.blade.php" })
+
+-- Additional autocommand to switch back to 'blade' after LSP has attached
+autocmd("LspAttach", function(args)
+  vim.schedule(function()
+    -- Check if the attached client is 'intelephense'
+    for _, client in ipairs(vim.lsp.get_active_clients()) do
+      if client.name == "intelephense" and client.attached_buffers[args.buf] then
+        vim.api.nvim_buf_set_option(args.buf, "filetype", "blade")
+        -- update treesitter parser to blade
+        vim.api.nvim_buf_set_option(args.buf, "syntax", "blade")
+        break
+      end
+    end
+  end)
+end, { pattern = "*.blade.php" }) ]]
 
 -- make $ part of the keyword for php.
 vim.api.nvim_exec([[ autocmd FileType php set iskeyword+=$ ]], false)
