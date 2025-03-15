70006431f32775e91a0e7a08d9cd89b30261eaf7
Stéphan Zych <szych@cbtw.tech>
2025-03-15 21:54:33 +0100

    Update LSP with garbage

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -20,4 +20,15 @@ return {
       },
     },
   },
+  -- Garbage collector that stops inactive LSP clients to free RAM
+  -- https://github.com/Zeioth/garbage-day.nvim
+  {
+    "zeioth/garbage-day.nvim",
+    dependencies = "neovim/nvim-lspconfig",
+    event = "VeryLazy",
+    opts = {
+      excluded_lsp_clients = {},
+      notifications = true,
+    },
+  },
 }
