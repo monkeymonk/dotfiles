628262d2017e79d9ea5ebaf0dd3480ddbc9c6e21
Stéphan Zych <szych@cbtw.tech>
2025-03-24 08:47:37 +0100

    Update obsidian support

--- .config/nvim/lua/plugins/notes.lua
+++ .config/nvim/lua/plugins/notes.lua
@@ -169,4 +169,38 @@ return {
       },
     },
   },
+
+  -- Obsidian 🤝 Neovim
+  -- https://github.com/epwalsh/obsidian.nvim
+  {
+    "epwalsh/obsidian.nvim",
+    version = "*", -- recommended, use latest release instead of latest commit
+    lazy = true,
+    config = true,
+    -- ft = "markdown",
+    -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+    event = {
+      -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+      -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+      -- refer to `:h file-pattern` for more examples
+      "BufReadPre "
+        .. vim.fn.expand("~")
+        .. "/.config/obsidian/monkeymonk/*.md",
+      "BufNewFile " .. vim.fn.expand("~") .. "/.config/obsidian/monkeymonk/*.md",
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-telescope/telescope.nvim",
+      "hrsh7th/nvim-cmp",
+    },
+    opts = {
+      workspaces = {
+        {
+          name = "monkeymonk",
+          path = vim.fn.expand("~") .. "/.config/obsidian/monkeymonk",
+        },
+      },
+    },
+  },
 }
