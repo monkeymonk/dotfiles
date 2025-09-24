77248922d47e86383ffa7d1d307ec22019514289
Stéphan Zych <szych@cbtw.tech>
2025-09-24 11:32:11 +0200

    Cleanup nvim config

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -37,3 +37,33 @@ if vim.g.neovide then
   vim.g.neovide_cursor_trail_length = 0 -- Disable cursor trail effect.
   vim.g.neovide_scale_factor = 0.8 -- Set the scaling factor for the Neovide window.
 end
+
+-- Filetypes
+-- Set .aliases or any variant as "bash"
+vim.filetype.add({ pattern = { ["%.*aliases.*"] = "bash" } })
+
+-- Set .zprofile or any variant as "bash"
+vim.filetype.add({ pattern = { ["%.*zprofile.*"] = "bash" } })
+
+-- Set .env or any variant as "dotenv" except we want .env.example files to be
+-- treated differently so it's easier to know we are editing an example file.
+-- This has less syntax highlighting and comes up with a different explorer
+-- icon so it's easier to know it's not the "real" env file.
+vim.filetype.add({
+  pattern = {
+    ["%.env.*"] = "dotenv",
+    ["%.env.*.example"] = { "conf", { priority = 1 } },
+  },
+})
+
+-- Ensure all .env files and variants are syntax highlighted as shell scripts.
+-- This goes along with the filetype addition above. We do this to avoid using
+-- bash or sh so the BashLSP linter and formatter doesn't affect env files
+-- since that can create a lot of unwanted false positives and side effects.
+vim.treesitter.language.register("bash", "dotenv")
+
+-- Set git config variarts as "git_config".
+vim.filetype.add({ pattern = { [".*/git/config.*"] = "git_config" } })
+
+-- Set requirements-lock.txt or any variant as "requirements".
+vim.filetype.add({ pattern = { ["requirements.*.txt"] = "requirements" } })
