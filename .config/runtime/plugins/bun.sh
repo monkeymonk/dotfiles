d46de4dbf9fd8541ccb2e521b0cff64f3f092323
Stéphan Zych <stephan@zych.be>
2026-03-03 09:14:45 +0100

    fix(runtime): add missing bun completion

--- .config/runtime/plugins/bun.sh
+++ .config/runtime/plugins/bun.sh
@@ -1,12 +1,17 @@
 # Bun environment.
 
 runtime_plugin_bun() {
-    if [ -d "$HOME/.bun" ] || has_cmd bun; then
-        export BUN_INSTALL="$HOME/.bun"
-        if [ -d "$BUN_INSTALL/bin" ]; then
-            path_prepend "$BUN_INSTALL/bin"
-        fi
+  if [ -d "$HOME/.bun" ] || has_cmd bun; then
+    export BUN_INSTALL="$HOME/.bun"
+    if [ -d "$BUN_INSTALL/bin" ]; then
+      path_prepend "$BUN_INSTALL/bin"
     fi
+
+    # bun completions
+    if [ -s "$BUN_INSTALL/_bun" ]; then
+      source "$BUN_INSTALL/_bun"
+    fi
+  fi
 }
 
 hook_register setup runtime_plugin_bun
